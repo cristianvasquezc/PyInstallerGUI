b324b58ee0648be6c4103ad6ff3577e592d2f323
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-01-01 13:40:09 -0500

    Modificar método center_window para usar self y actualizar la geometría de la ventana correctamente

--- main.py
+++ main.py
@@ -35,15 +35,15 @@ class BuildWindow(tk.Toplevel):
 
         threading.Thread(target=self.run_build, args=(command,), daemon=True).start()
 
-    def center_window(root, width, height):
-        root.update_idletasks()
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
+    def center_window(self, width, height):
+        self.update_idletasks()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
         
         x = (screen_width // 2) - (width // 2)
         y = (screen_height // 2) - (height // 2)
         
-        root.geometry(f"{width}x{height}+{x}+{y}")
+        self.geometry(f"{width}x{height}+{x}+{y}")
 
     # -------- helper para escribir logs --------
     def log(self, text):
