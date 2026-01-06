5515b5075e7346057f361523a21eeb3e4660e322
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-01-06 11:16:43 -0500

    feat: Corregir parámetros de las funciones center_window y set_app_icon para usar 'self'

--- src/utils.py
+++ src/utils.py
@@ -2,16 +2,17 @@ import subprocess
 import shutil
 import sys
 import os
+import tkinter as tk
 
-def center_window(root, width, height):
-    root.update_idletasks()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+def center_window(self, width, height):
+    self.update_idletasks()
+    screen_width = self.winfo_screenwidth()
+    screen_height = self.winfo_screenheight()
     
     x = (screen_width // 2) - (width // 2)
     y = (screen_height // 2) - (height // 2)
     
-    root.geometry(f"{width}x{height}+{x}+{y}")
+    self.geometry(f"{width}x{height}+{x}+{y}")
 
 def resource_path(rel_path):
     try:
@@ -20,14 +21,20 @@ def resource_path(rel_path):
         base = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
     return os.path.join(base, rel_path)
 
-def set_app_icon(window, icon_name="assets/icon.ico"):
+def set_app_icon(self, icon_name="assets/icon.ico"):
     from tkinter import TclError
     icon_path = resource_path(icon_name)
     if os.path.exists(icon_path):
         try:
-            window.iconbitmap(icon_path)
+            self.iconbitmap(icon_path)
         except TclError:
             pass
+
+# Monkey-patching
+tk.Tk.center_window = center_window
+tk.Toplevel.center_window = center_window
+tk.Tk.set_app_icon = set_app_icon
+tk.Toplevel.set_app_icon = set_app_icon
 
 def is_python_installed():
     try:
